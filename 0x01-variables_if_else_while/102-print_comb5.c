511374ff6fbc3746fef029c21e458450f0711623
SylexMS <rahhalisiyatil@gmail.com>
2023-03-20 22:07:24 -0700

    _putchar

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -37,5 +37,6 @@ int main(void)
 		firstDijit++;
 	}
 	putchar('\n');
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print a string with print function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
