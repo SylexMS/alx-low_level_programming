b2de7f37118ead9d10792babc0006c4b0072b0f6
SylexMS <rahhalisiyatil@gmail.com>
2023-03-21 14:44:49 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - utilizes on the _putchar function to print
+ *the aphabet in lowercase.
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
