4a164317d01074922464b42e7117e0431c316eb5
SylexMS <rahhalisiyatil@gmail.com>
2023-03-23 18:47:05 -0700

    i believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,8 +13,7 @@ void print_most_numbers(void)
 
 	for (num = 0 ; num <= 9 ; num++)
 	{
-		if (num == 2 && num == 4)
-			continue;
+		if (!(num == 2  || num == 4))
 		_putchar(num + 48);
 	}
 	_putchar('\n');
