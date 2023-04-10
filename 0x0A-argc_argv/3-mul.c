79e80183fc2d8f2b8040c8f181ab03fbb2ec5e77
SylexMS <rahhalisiyatil@gmail.com>
2023-04-10 14:10:00 -0700

    mul arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of commandline arg
+ * @argv: pointer to an array
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
