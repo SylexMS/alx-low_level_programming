ae55154f163497bd51950fc03300980b925022a8
SylexMS <rahhalisiyatil@gmail.com>
2023-04-10 13:54:15 -0700

    wc arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argv: number of arg passed
+ * @argc: pointer to an array of command line arg
+ *
+ * Return: Awlays 0
+*/
+
+int main(int argc, char *argv[])
+{
+	void(*argv[]);
+	printf("%d\n", argc - 1);
+	return (0);
+}
