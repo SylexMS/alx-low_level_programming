0f32bc2c257324fc8b6e7fec4f15cbb51997674f
SylexMS <rahhalisiyatil@gmail.com>
2023-04-10 13:33:56 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ * @argv: ls
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
