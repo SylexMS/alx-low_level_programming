47e2b33a02bb538cace5e938521850d2adbd0b8e
SylexMS <rahhalisiyatil@gmail.com>
2023-04-10 14:00:34 -0700

    print all wr

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ * @argv: ls
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int x = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[x]);
+		i++;
+	}
+	return (0);
+}
