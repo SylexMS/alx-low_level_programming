f33e9b41c51903e9588fd83a47b16e44b0187996
SylexMS <rahhalisiyatil@gmail.com>
2023-04-27 14:38:57 -0700

    add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	if (str)
 	{
-		new_node->str = strup(str);
+		new_node->str = strdup(str);
 		if (!new_node->str)
 		{
 			free(new_node);
