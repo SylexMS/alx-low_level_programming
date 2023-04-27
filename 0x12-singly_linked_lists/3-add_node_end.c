f9ab031fab115b7d07f3d9bc8f3a92b437630708
SylexMS <rahhalisiyatil@gmail.com>
2023-04-27 14:36:27 -0700

    add_node_end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node to the end of list
+ * @head: address of ptr to head node
+ * @str: str field of node
+ *
+ * Return: size of list
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !new_head)
+		return (NULL);
+	if (str)
+	{
+		new_node->str = strup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = nod->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
