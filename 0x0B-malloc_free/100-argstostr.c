336126763ce886886306700a00f4928e6d3c4834
SylexMS <rahhalisiyatil@gmail.com>
2023-06-13 12:46:47 -0700

    argstotstr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,13 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * argstostr - a function that concatenates all the arg of prg
+ * _strlen - find lenght of a str
+ * @s: str
+ * Return: int
+*/
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0' ; size++)
+	;
+	return (size);
+}
+
+/**
+ * *argstostr - description
  * @ac: int
- * @av: char
- * Return: av
+ * @av: arg
+ * Return: str
 */
 
 char *argstostr(int ac, char **av)
 {
+	int i = 0, nc = 0, j = 0, cmp = 0;
+	char *s;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (; i < ac ; i++ ; nc++)
+		nc += _strlen(av[i]));
+
+	s = malloc(sizeof(char) * nc + 1);
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++, cmp++)
+			s[cmp] = av[i][j];
+
+		s[cmp] = '\n';
+		cmp++;
+	}
+	s[cmp] = '\0';
+	return (s);
+}
