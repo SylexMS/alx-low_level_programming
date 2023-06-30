8cae2cec9154c587b4a7038cda90a1a07182004f
SylexMS <rahhalisiyatil@gmail.com>
2023-06-30 08:08:14 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
 
-	if (!head || !new_head)
+	if (!head || !new_node)
 		return (NULL);
 	if (str)
 	{
@@ -21,14 +21,14 @@ list_t *add_node_end(list_t **head, const char *str)
 		if (!new_node->str)
 		{
 			free(new_node);
-			return (NULL);
+			return (NULL):
 		}
 		new_node->len = _strlen(new_node->str);
 	}
 	if (node)
 	{
 		while (node->next)
-			node = nod->next;
+			node = node->next;
 		node->next = new_node;
 	}
 	else
