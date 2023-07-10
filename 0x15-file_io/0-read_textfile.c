32b4b2b74d51075b8d0294b2014ec373cc8f9955
SylexMS <rahhalisiyatil@gmail.com>
2023-07-10 06:36:49 -0700

    cp

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int frd;
-	ssize_t bts;
+	int fd;
+	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	frd = open(filename, O_RDONLY);
-	if (frd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	bts = read(fd, &buf[0], letters);
-	bts = write(STDOUT_FILENO, &buf[0], bts);
-	close(frd);
-	return (bts);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,16 +27,16 @@ int _strlen(char *c)
 
 int create_file(const char *filename, char *text_content)
 {
-	int frd;
-	ssize_t bts = 0, len = _strlen(text_content);
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	frd = open(filename. 0_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (frd == -1)
+	fd = open(filename. 0_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 	if (len)
-		bts = write(frd, text_content, len);
-	close(frd);
-	return (bts == len ? 1 : -1);
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
